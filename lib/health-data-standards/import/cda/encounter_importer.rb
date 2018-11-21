219d6d8d4db744d0db361ce2c6e96a8850b95135
Giriraj Kashyap <giriraj.kashyapa@gmail.com>
2018-11-21 11:44:44 -0600

    Update encounter_importer.rb

--- lib/health-data-standards/import/cda/encounter_importer.rb
+++ lib/health-data-standards/import/cda/encounter_importer.rb
@@ -65,6 +65,9 @@ module HealthDataStandards
         def extract_discharge_disposition(parent_element, encounter)
           encounter.discharge_time = encounter.end_time
           encounter.discharge_disposition = extract_code(parent_element, "./sdtc:dischargeDispositionCode")
+          puts encounter.discharge_disposition
+          puts encounter.discharge_disposition.code
+          puts encounter.discharge_disposition["code"]
         end
 
         def extract_transfer(transfer_element, encounter)
